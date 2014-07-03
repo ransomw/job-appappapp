0170f0e83b0fdadf7bec006a6a42082ffe768c0a
Auvergne <auvergnerw@gmail.com>
2014-07-03 18:10:21 +0800

    start implementing models and views

--- flask-sand/jobapp/views.py
+++ flask-sand/jobapp/views.py
@@ -1,6 +1,12 @@
+from flask import url_for
+from flask import render_template
+
 from jobapp import app
 
 @app.route('/hello')
 def hello():
     return 'Hello World.'
 
+@app.route('/companies/new')
+def create_company():
+    return render_template('new_company.html')
--- flask-sand/runjobapp.py
+++ flask-sand/runjobapp.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python
 
+from optparse import OptionParser
 import sys
 
 from jobapp import app
-# from jobapp.database import init_db
+from jobapp.database import init_db
 
-# if 'initdb' in sys.argv:
-#     init_db()
+parser = OptionParser()
+parser.add_option("-d", "--init-db",
+                  action='store_true', dest='is_initdb', default=False,
+                  help="initialize a new database before starting")
+(options, args) = parser.parse_args()
+
+if options.is_initdb:
+    init_db()
 
 app.run(host='0.0.0.0')
