a59a776b1f4437b2f2c6389c80cea5eaa2c5ed90
Auvergne <auvergnerw@gmail.com>
2014-09-07 10:02:37 -0500

    template for company detail

--- hoodie-sand/www/js/views.js
+++ hoodie-sand/www/js/views.js
@@ -2,27 +2,35 @@ define([
     'handlebars',
     'bower_components/lodash/dist/lodash.min',
     'js/hbs-helpers',
-    'text!views/company-list.hbs'
+    'text!views/company-list.hbs',
+    'text!views/company-detail.hbs'
 ], function (
     Handlebars, _, hbs_helpers,
-    company_list_t) {
+    company_list_t, company_detail_t) {
     "use strict";
 
     Handlebars.registerHelper('list', hbs_helpers.list);
 
     var company_list_ct = Handlebars.compile(company_list_t);
+    var company_detail_ct = Handlebars.compile(company_detail_t);
 
     var company_list = function(companies) {
         return company_list_ct({companies: companies});
     };
 
     var company_detail = function(company) {
+
+        return company_detail_ct(company);
+
+/*
         return [
             '<h2>',
             "company detail view for ",
             company.name,
             '</h2>'
         ].join('');
+*/
+
     };
 
     return {
