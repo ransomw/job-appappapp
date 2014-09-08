bafb5a6dcb8a521063fa5ee8819d30cdd605a49b
Auvergne <auvergnerw@gmail.com>
2014-09-08 07:01:34 -0500

    lint CompanyForm

--- react-sand/js/components/CompanyForm.react.js
+++ react-sand/js/components/CompanyForm.react.js
@@ -1,21 +1,30 @@
+/*global define: false */
+
 define([
     'react',
-    'lodash',
     'actions/ViewActions',
-], function(React, _, actions) {
+], function (React, actions) {
+    "use strict";
 
     var CompanyForm = React.createClass({
 
-        on_save_click: function(event) {
+        // function (event)
+        on_save_click: function () {
             var company_name = this.refs.name.getDOMNode().value.trim();
             actions.create_company({name: company_name});
         },
 
 
-        render: function() {
+        render: function () {
             return React.DOM.div(null, [
-                React.DOM.input({type: 'text', placeholder: "company name", ref: 'name', key: 'name'}, {}),
-                React.DOM.input({type: 'submit', value: 'Save', onClick: this.on_save_click, key: 'submit'}, {})
+                React.DOM.input({type: 'text',
+                                 placeholder: "company name",
+                                 ref: 'name',
+                                 key: 'name'}, {}),
+                React.DOM.input({type: 'submit',
+                                 value: 'Save',
+                                 onClick: this.on_save_click,
+                                 key: 'submit'}, {})
             ]);
         }
     });
