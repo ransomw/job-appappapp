e0a9ae5fa59d6d07c582f3ed75fd7c370d435507
ransom <auvergnerw@gmail.com>
2014-09-08 04:41:57 -0500

    CompanyList lint clean

--- react-sand/js/components/CompanyList.react.js
+++ react-sand/js/components/CompanyList.react.js
@@ -1,16 +1,20 @@
+/*global define: true */
+/*jslint nomen: true */
+
 define([
     'react',
-    'lodash',
-		'actions/ViewActions'
-], function(React, _, actions) {
+    'actions/ViewActions'
+], function (React, actions) {
+    "use strict";
 
     var ListItem = React.createClass({
 
-        on_sel_click: function (ev) {
+        // function (ev)
+        on_sel_click: function () {
             actions.company_select(this.props.company);
         },
 
-        render: function() {
+        render: function () {
             return React.DOM.tr(null, [
                 React.DOM.td({key: 'name'}, [
                     React.DOM.a({
@@ -19,20 +23,21 @@ define([
                 ])
             ]);
         }
-    });
+    }),
 
-    var CompanyList = React.createClass({
-        render: function() {
-            var all_companies = this.props.all_companies;
-            var companies = [];
+        CompanyList = React.createClass({
+            render: function () {
+                var all_companies = this.props.all_companies,
+                    companies = [],
+                    idx;
 
-            for (var idx in all_companies) {
-                companies.push(ListItem({key: idx, company: all_companies[idx]}));
+                for (idx in all_companies) {
+                    companies.push(ListItem({key: idx, company: all_companies[idx]}));
+                }
+
+                return React.DOM.table(null, companies);
             }
-
-            return React.DOM.table(null, companies);
-        }
-    });
+        });
 
     return CompanyList;
 });
