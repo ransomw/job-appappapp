7b378df0c5ea40f464edb695a164b81e28bc4742
ransom <auvergnerw@gmail.com>
2014-09-18 02:06:15 -0500

    placeholder account status view (for logout button)

---
+++ hoodie-react/www/js/components/AccountStatus.react.js
@@ -0,0 +1,15 @@
+/*global define: false */
+
+define([
+    'react'
+], function (React) {
+    "use strict";
+
+    var AccountStatus = React.createClass({
+        render: function () {
+            return React.DOM.h3(null, "account status view unimplemented");
+        }
+    });
+
+    return AccountStatus;
+});
--- hoodie-react/www/js/components/CareerApp.react.js
+++ hoodie-react/www/js/components/CareerApp.react.js
@@ -3,21 +3,25 @@
 define([
     'js/stores/AccountStore',
     'react',
-    'components/LoginForm.react'
-], function (AccountStore, React, LoginForm) {
+    'components/LoginForm.react',
+    'components/AccountStatus.react'
+], function (AccountStore, React,
+             LoginForm, AccountStatus) {
     "use strict";
 
     var get_app_state = function () {
-        var logged_in;
+        var logged_in,
+            username = AccountStore.get_login_state();
 
-        if (AccountStore.get_login_state() === undefined) {
+        if (username === undefined) {
             logged_in = false;
         } else {
             logged_in = true;
         }
 
         return {
-            logged_in: logged_in
+            logged_in: logged_in,
+            username: username
         };
     },
 
@@ -37,7 +41,11 @@ define([
                         new LoginForm({key: 'LoginForm'})
                     ]);
                 }
-                return React.DOM.h3(null, "logged in view unimplemented");
+                return React.DOM.div(null, [
+                    new AccountStatus({key: 'AccountStatus',
+                                       username: this.state.username}),
+                    React.DOM.h3(null, "logged in view unimplemented")
+                    ]);
             },
 
             _onAccountChange: function () {
