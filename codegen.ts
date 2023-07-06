3b2dace0260594ecd160188302e0c7a9b1d31d28
Ransom Williams <auvergnerw@gmail.com>
2023-07-06 14:05:10 -0500

    init codegen

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+
+const config: CodegenConfig = {
+  schema: 'schema.graphql',
+  documents: ['src/**/*.{ts,tsx}'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      }
+    }
+  },
+  ignoreNoDocuments: true,
+};
+
+
+export default config;
--- server.py
+++ server.py
@@ -4,12 +4,16 @@ from flask import jsonify
 from flask import render_template
 import graphene
 from flask_graphql import GraphQLView
+from graphql.utils import schema_printer
+import click
+
 
 class Query(graphene.ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `first_name`
     # By default, the argument name will automatically be camel-based into firstName in the generated schema
     hello = graphene.String(first_name=graphene.String(default_value="stranger"))
     goodbye = graphene.String()
+    whatup = graphene.String()
 
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (first_name) for the Field and returns data for the query Response
@@ -41,10 +45,28 @@ app.add_url_rule(
     )
 )
 
-werkzeug.serving.run_simple(
-    "0.0.0.0",
-    5000,
-    app,
-    use_debugger=True,
-    use_reloader=True,
-)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def gql_schema():
+    my_schema_str = schema_printer.print_schema(schema)
+    with open("schema.graphql", "w") as fp:
+        fp.write(my_schema_str)
+        fp.close()
+
+@cli.command()
+def run_app():
+    werkzeug.serving.run_simple(
+        "0.0.0.0",
+        5000,
+        app,
+        use_debugger=True,
+        use_reloader=True,
+    )
+
+
+if __name__ == '__main__':
+    cli()
